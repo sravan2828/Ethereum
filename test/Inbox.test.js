11cd162fd603538ea353c55309c8c01523b8ea03
sravan kumar v <sravan_vempadapu@intuit.com>
2018-07-11 19:46:03 +0530

    ganache fetched accounts to deploy contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,26 +4,15 @@ const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
 
-class Car {
-    park() {
-        return "stopped";
-    }
-    drive() {
-        return "vroom";
-    }
-}
-
-let car;
-
 beforeEach(()=>{
-    car = new Car();
+    web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+        console.log(fetchedAccounts);
+    });
 })
 
-describe('car', () => {
-    it('can park',()=>{
-        assert.equal(car.park(), 'stopped');
-    });
-    it('can drive',()=>{
-        assert.equal(car.drive(), 'vroom');
+describe('Inbox', () => {
+    it('deploys a contract',()=>{
+        
     });
 });
